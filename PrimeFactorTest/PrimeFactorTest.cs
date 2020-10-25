4ec393e591ef075161e8be5ecea053eb3ec2cf16
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-25 21:52:24 +1100

    Add the second test

--- PrimeFactorTest/PrimeFactorTest.cs
+++ PrimeFactorTest/PrimeFactorTest.cs
@@ -6,15 +6,27 @@ namespace PrimeFactorTest
 {
     public class PrimeFactorTest
     {
+        private static List<int> List(params int[] ints)
+        {
+           var list =  new List<int>();
+           foreach (var item in ints)
+           {
+               list.Add(item);
+           }
+
+           return list;
+        }
+        
         [Fact]
         public void Test1()
         {
             Assert.Equal(List(), PrimeFactors.Generate(1));
         }
-
-        private static List<int> List()
+        
+        [Fact]
+        public void Test2()
         {
-            return new List<int>();
+            Assert.Equal(List(2), PrimeFactors.Generate(2));
         }
     }
 }
